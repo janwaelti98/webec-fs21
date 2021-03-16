4d17cc50ac04f5eaa2ea06af77e4958fa668658c
Michael Faes <rolve@trick17.ch>
2021-03-16 08:32:29 +0100

    Template for Thymeleaf exercises

---
+++ contactlist-thymeleaf/src/test/java/webeng/contactlist/service/ContactServiceTest.java
@@ -0,0 +1,62 @@
+package webeng.contactlist.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+import webeng.contactlist.model.ContactListEntry;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactServiceTest {
+
+    ContactService service;
+
+    ContactServiceTest() throws IOException {
+        var mapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.service = new ContactService(mapper);
+    }
+
+    @Test
+    void contactListIds() {
+        var contactList = service.getContactList();
+        assertNotNull(contactList);
+        var ids = contactList.stream()
+                .map(ContactListEntry::getId)
+                .collect(toList());
+        assertEquals(rangeClosed(1, 30).boxed().collect(toList()), ids);
+    }
+
+    @Test
+    void contactListName() {
+        var contactList = service.getContactList();
+        assertNotNull(contactList);
+        assertFalse(contactList.isEmpty());
+        assertEquals("Mabel Guppy", contactList.get(0).getName());
+    }
+
+    @Test
+    void findContact() {
+        var contact = service.findContact(1).orElseThrow(AssertionFailedError::new);
+        assertEquals(1, contact.getId());
+        assertEquals("Mabel", contact.getFirstName());
+        assertEquals("Guppy", contact.getLastName());
+        assertEquals(List.of("405-580-6403"), contact.getPhone());
+        assertEquals(emptyList(), contact.getEmail());
+        assertEquals("Librarian", contact.getJobTitle());
+        assertEquals("Photolist", contact.getCompany());
+    }
+
+    @Test
+    void findContactAbsent() {
+        var contact = service.findContact(98213);
+        assertTrue(contact.isEmpty());
+    }
+}
