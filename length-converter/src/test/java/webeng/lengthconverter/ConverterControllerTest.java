838bf9aea0ca7f03fce1682f79bbe539d898a8b2
Michael Faes <rolve@trick17.ch>
2021-03-17 13:01:31 +0100

    Solution for length-converter exercises

---
+++ length-converter/src/test/java/webeng/lengthconverter/ConverterControllerTest.java
@@ -0,0 +1,40 @@
+package webeng.lengthconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConverterControllerTest {
+
+    ConverterController controller = new ConverterController();
+    Model model = new ConcurrentModel();
+
+    @Test
+    public void testConvertView() {
+        var view = controller.convert(10, 10, model);
+        assertEquals("convert", view);
+    }
+
+    @Test
+    public void testModelFeetInches() {
+        controller.convert(5, 10, model);
+        assertEquals(5, model.getAttribute("feet"));
+        assertEquals(10, model.getAttribute("inches"));
+    }
+
+    @Test
+    public void testConvertSimple() {
+        var view = controller.convert(5, 10, model);
+        assertEquals(177, model.getAttribute("cmPart"));
+        assertEquals(8, model.getAttribute("mmPart"));
+    }
+
+    @Test
+    public void testConvertSpecialCase0() {
+        var view = controller.convert(0, 0, model);
+        assertEquals(0, model.getAttribute("cmPart"));
+        assertEquals(0, model.getAttribute("mmPart"));
+    }
+}
